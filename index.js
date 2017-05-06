e31bb79b96c42d2b32b5112fc8dd3309c956d4b2
Youssefares <Youssefares@ymail.com>
2017-05-06 21:51:30 +0200

    added dependencies & tested webhook & message handling

---
+++ index.js
@@ -0,0 +1,89 @@
+//loading environment variables
+const dotenv = require('dotenv');
+dotenv.load();
+
+//dependencies
+const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
+const {WitMessengerBot, BotSessionsDelegate} = require('wit-messenger-bot')
+
+
+const actions = {
+  //wit.ai defined actions go here
+  //TODO: populate this as we build stories
+}
+
+
+//WitMessengerBot instance
+let bot = new WitMessengerBot({
+  token: process.env.PAGE_ACCESS_TOKEN,
+  verify: process.env.VERIFY_TOKEN
+},{
+  accessToken: process.env.WIT_ACCESS_TOKEN,
+  actions: actions
+}, new BotSessionsDelegate())
+
+
+//handling errors
+bot.on('error', (err) => {
+    console.log("botError: "+err.message)
+})
+
+//handling messages
+bot.on('message', (payload, reply) => {
+    let text = payload.message.text
+    let senderId = payload.sender.id
+    let {sessionId, sessionData} = bot.findOrCreateSession(senderId)
+    let context = JSON.parse(sessionData)
+
+
+    //some interaction..
+    //let user know the bot has seen the message
+    bot.sendSenderAction(senderId, 'mark_seen', function(err, reply) {
+        if (err) throw err
+    })
+
+    //let user know the bot is typing..
+    bot.sendSenderAction(senderId, 'typing_on', function(err, reply) {
+        if (err) throw err
+    })
+
+    bot.runActions(sessionId, text, context, (context) => {
+      //conversation context logic
+      context = {}
+
+      //delete if(empty object), else update.
+      if (Object.keys(context).length === 0 && context.constructor === Object){
+        bot.deleteSession(sessionId)
+      }
+      else bot.writeSession(sessionId, JSON.stringify(context))
+
+    }).catch(function(error){
+			console.log(error)
+		})
+})
+
+
+//initing express app
+let app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+//Configing the way the server handles requests
+app.get('/', (req, res) => {
+    //when facebook hits this webhook with a GET, return verify token specified in bot instance.
+    return bot._verify(req, res)
+})
+
+app.post('/', (req, res) => {
+    bot._handleMessage(req.body)
+    res.end(JSON.stringify({
+        status: 'ok'
+    }))
+})
+
+//creating server
+http.createServer(app).listen(process.env.PORT)
